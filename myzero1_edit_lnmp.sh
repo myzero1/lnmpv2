47d8562fa3730d2556a4d1195722f93b853b1666
myzero1 <myzero1@sina.com>
2016-09-21 22:55:45 +0800

    Update myzero1_edit_lnmp.sh

--- myzero1_edit_lnmp.sh
+++ myzero1_edit_lnmp.sh
@@ -9,3 +9,6 @@ sed -i "s|DBSelect=\"2\"|DBSelect=\"3\"|g" include/main.sh
 sed -i "s|OLDCONFIG=`stty -g`|#OLDCONFIG=`stty -g`|g" include/main.sh
 sed -i "s|stty -icanon -echo min 1 time 0|#stty -icanon -echo min 1 time 0|g" include/main.sh
 sed -i "s|dd count=1 2\>\/dev\/null|dd count=1 2\>\/dev\/null|g" include/main.sh
+
+
+bash /opt/source/lnmp1.3/install.sh
