aacd37cff148890fdb7cf8768d374ccad94adf3f
myzero1 <myzero1@sina.com>
2016-09-25 20:09:12 +0800

    Update auto_start.sh

--- auto_start.sh
+++ auto_start.sh
@@ -1,3 +1,16 @@
+#add log dir for lnmp
+if [ ! -d "/var/log/nginx" ]; then
+  mkdir /var/log/nginx
+fi
+
+if [ ! -d "/var/log/mysql" ]; then
+  mkdir /var/log/mysql
+fi
+
+if [ ! -d "/var/log/nginx" ]; then
+  mkdir /var/log/mysql
+fi
+
 
 /etc/init.d/nginx start
 /etc/init.d/php-fpm start
