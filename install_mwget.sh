b49774416a64efb36cef3152d27d285d7d6a3704
myzero1 <myzero1@sina.com>
2016-10-30 16:24:51 +0800

    Update install_mwget.sh

--- install_mwget.sh
+++ install_mwget.sh
@@ -1,13 +1,16 @@
-cd /usr/local/src/
+mkdir -p /opt/source/mwget
+cd /opt/source/mwget
 
-wget http://d.139.sh/2250743895/mwget_0.1.0.orig.tar.bz2
+wget http://d.139.sh/2250743895/ez_setup.py
+python ez_setup.py
 
-tar -xvf mwget_0.1.0.orig.tar.bz2
+wget http://d.139.sh/2250743895/pip-1.5.5.tar.gz
+tar zvxf 1.5.5.tar.gz
+cd pip-1.5.5/
+python setup.py install
 
-cd mwget_0.1.0.orig
-
-./configure
-
-make
-
-make install
+cd ..
+wget http://d.139.sh/2250743895/mwget-master.zip
+uzip mwget-master.zip
+cd mwget-master
+pip install mwget
