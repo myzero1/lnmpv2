adbbfdd7b5ad39b988ea9a77f8d8ba14540a0a18
myzero1 <myzero1@sina.com>
2016-10-30 19:15:06 +0800

    Update install_mwget.sh

--- install_mwget.sh
+++ install_mwget.sh
@@ -1,9 +1,14 @@
 mkdir -p /opt/source/mwget
 cd /opt/source/mwget
 
-wget -q http://d.139.sh/2250743895/ez_setup.py
+#wget -q http://d.139.sh/2250743895/ez_setup.py
 #wget -q https://bootstrap.pypa.io/ez_setup.py
-python ez_setup.py
+#python ez_setup.py
+
+wget http://d.139.sh/2250743895/setuptools-28.7.1.zip
+uzip setuptools-28.7.1.zip
+cd setuptools-28.7.1.zip
+python setup.py install
 
 wget http://d.139.sh/2250743895/pip-1.5.5.tar.gz
 tar zvxf pip-1.5.5.tar.gz
